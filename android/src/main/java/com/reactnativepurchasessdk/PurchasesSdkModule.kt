2a6a85844466c0393248a84293cf27a27e3fb38c
Tan Nghia La <nghiala@live.de>
2021-10-13 13:31:57 +0200

    added android purchase and receipt verification

--- android/src/main/java/com/reactnativepurchasessdk/PurchasesSdkModule.kt
+++ android/src/main/java/com/reactnativepurchasessdk/PurchasesSdkModule.kt
@@ -10,6 +10,7 @@ import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
 import kotlinx.coroutines.*
 import java.util.*
 
@@ -77,8 +78,13 @@ class PurchasesSdkModule(reactContext: ReactApplicationContext) : ReactContextBa
         reactApplicationContext.currentActivity?.let {
           try {
             LinkFivePurchases.purchase(productId, activity = it, callback = { result ->
-              val succeeded = result.getOrThrow()
-              promise.resolve(succeeded)
+              try {
+                val succeeded = result.getOrThrow()
+                promise.resolve(succeeded)
+              } catch (cause: Throwable) {
+                Logger.e(cause.localizedMessage)
+                promise.reject(cause)
+              }
             })
           } catch (cause: Throwable) {
             Logger.e(cause.localizedMessage)
@@ -97,11 +103,42 @@ class PurchasesSdkModule(reactContext: ReactApplicationContext) : ReactContextBa
 
     @ReactMethod
     fun fetchReceiptInfo(fromCache: Boolean, promise: Promise) {
+      Logger.v("Start to fetch receipts")
 
+      try {
+        LinkFivePurchases.fetchReceiptInfo(callback = { receiptResult ->
+          try {
+            val receipts = receiptResult.getOrThrow()
+
+            val resultingReceipts = WritableNativeArray()
+            receipts.forEach {
+              val map = Arguments.createMap()
+              map.putString("sku", it.sku)
+              map.putString("purchaseId", it.purchaseId)
+              map.putString("transactionDate", it.transactionDate)
+              map.putString("validUntilDate", it.validUntilDate)
+              map.putBoolean("isTrial", it.isTrial)
+              map.putBoolean("isExpired", it.isExpired)
+              map.putString("familyName", it.familyName)
+              map.putString("attributes", it.attributes)
+              map.putString("period", it.period)
+              resultingReceipts.pushMap(map)
+            }
+
+            promise.resolve(resultingReceipts)
+          } catch (cause: Throwable) {
+            Logger.e(cause.localizedMessage)
+            promise.reject(cause)
+          }
+        })
+      } catch (cause: Throwable) {
+        Logger.e(cause.localizedMessage)
+        promise.reject(cause)
+      }
     }
 }
 
-object LinkFivePurchases {
+object LinkFivePurchases: PurchasesResponseListener {
 
   private lateinit var apiClient: LinkFiveApiClient
   private lateinit var billingClient: BillingClient
@@ -109,6 +146,7 @@ object LinkFivePurchases {
 
   private lateinit var fetchSubscriptionsCallback: (Result<List<LinkFiveProduct>>) -> Unit
   private lateinit var purchaseCallback: (Result<Boolean>) -> Unit
+  private lateinit var receiptCallback: (Result<List<LinkFiveVerifiedReceipt>>) -> Unit
 
   private val purchasesUpdatedListener =
     PurchasesUpdatedListener { billingResult, purchases ->
@@ -117,8 +155,7 @@ object LinkFivePurchases {
       if (billingResult.responseCode == BillingClient.BillingResponseCode.OK && !purchases.isNullOrEmpty()) {
 
         GlobalScope.launch {
-          // TODO: Send purchase to linkfive server
-          apiClient.verify()
+          handlePurchase(purchases)
         }
         purchaseCallback(Result.success(true))
       } else if (billingResult.responseCode == BillingClient.BillingResponseCode.USER_CANCELED) {
@@ -137,7 +174,7 @@ object LinkFivePurchases {
       .build()
   }
 
-  suspend fun purchase(productId: String, activity: Activity, callback: (Result<Boolean>) -> Unit) {
+  fun purchase(productId: String, activity: Activity, callback: (Result<Boolean>) -> Unit) {
     this.purchaseCallback = callback
 
     skuDetailList.find { it.sku == productId }?.let {
@@ -188,6 +225,11 @@ object LinkFivePurchases {
     })
   }
 
+  fun fetchReceiptInfo(callback: (Result<List<LinkFiveVerifiedReceipt>>) -> Unit) {
+    receiptCallback = callback
+    billingClient.queryPurchasesAsync(BillingClient.SkuType.SUBS, this)
+  }
+
   private suspend fun fetchLinkFiveProducts(skus: List<String>): List<LinkFiveProduct> {
     val params = SkuDetailsParams.newBuilder()
     params.setSkusList(skus).setType(BillingClient.SkuType.SUBS)
@@ -202,6 +244,45 @@ object LinkFivePurchases {
       return emptyList()
     }
 
+  }
+
+  private suspend fun handlePurchase(purchases: List<Purchase>) {
+    purchases.forEach { purchase ->
+      if (purchase.isAcknowledged.not()) {
+        Logger.d("Purchase not Acknowledged, will consume now")
+        val consumeParams =
+          AcknowledgePurchaseParams.newBuilder()
+            .setPurchaseToken(purchase.purchaseToken)
+            .build()
+        val consumeResult = billingClient.acknowledgePurchase(consumeParams)
+
+        Logger.d(
+          "Purchase consumed. " +
+            "code: ${consumeResult.responseCode} " +
+            "message: ${consumeResult.debugMessage}"
+        )
+      }
+    }
+
+    val verifiedPurchases = apiClient.verify(purchases)
+    receiptCallback(Result.success(verifiedPurchases))
+  }
+
+  override fun onQueryPurchasesResponse(p0: BillingResult, p1: MutableList<Purchase>) {
+    Logger.v("Query Purchase Response.")
+
+    GlobalScope.launch {
+      if (p0.responseCode == BillingClient.BillingResponseCode.OK && !p1.isNullOrEmpty()) {
+        handlePurchase(p1)
+      } else if (p0.responseCode == BillingClient.BillingResponseCode.USER_CANCELED) {
+        receiptCallback(Result.failure(UserCancelledPaymentException()))
+        Logger.d("User Canceled")
+        // Handle an error caused by a user cancelling the purchase flow.
+      } else {
+        Logger.d("Other error code: ${p0.responseCode} ${p0.debugMessage}. no purchase found")
+        receiptCallback(Result.failure(NoPurchaseFoundException()))
+      }
+    }
   }
 }
 
@@ -224,9 +305,7 @@ class LinkFiveApiClient(private val apiKey: String, private val environment: Lin
   suspend fun fetchSubscriptions(): LinkFiveSubscriptionList {
     try {
       val url = "${environment.url}/v1/subscriptions"
-      Log.d("TAG", url)
       val response: LinkFiveSubscriptionListResponse = client.get(url)
-      Log.d("TAG", response.toString())
 
       return response.data
     } catch (cause: Throwable) {
@@ -235,8 +314,18 @@ class LinkFiveApiClient(private val apiKey: String, private val environment: Lin
     }
   }
 
-  suspend fun verify() {
-    val response: HttpResponse = client.post("${environment.url}/v1/purchases/google/verify")
+  suspend fun verify(purchases: List<Purchase>): List<LinkFiveVerifiedReceipt> {
+    try {
+      val response: LinkFiveVerifiedPurchasesResponse = client.post("${environment.url}/v1/purchases/google/verify") {
+        contentType(ContentType.Application.Json)
+        body = LinkFiveVerifyPurchasesRequest(purchases)
+      }
+
+      return response.data.purchases
+    } catch (cause: Throwable) {
+      Log.d("TAG", cause.localizedMessage)
+      throw cause
+    }
   }
 }
 
@@ -268,6 +357,53 @@ data class LinkFiveProduct(
   val subscriptionPeriod: String
 )
 
+data class LinkFiveVerifyPurchasesRequest(
+  val purchases: List<LinkFiveVerifyPurchasesRequestPurchase>
+){
+  constructor(purchaseList: List<Purchase>, sameConstructorOverload: Boolean = false): this(
+    purchases = purchaseList.map {
+      LinkFiveVerifyPurchasesRequestPurchase(it)
+    }.toList()
+  )
+}
+
+data class LinkFiveVerifyPurchasesRequestPurchase(
+  val packageName: String,
+  val purchaseToken: String,
+  val orderId: String,
+  val purchaseTime: Long,
+  val sku: String
+) {
+
+  constructor(purchase: Purchase): this(
+    packageName = purchase.packageName,
+    purchaseToken = purchase.purchaseToken,
+    orderId = purchase.orderId,
+    purchaseTime = purchase.purchaseTime,
+    sku = purchase.skus.first()
+  )
+}
+
+data class LinkFiveVerifiedPurchasesResponse(
+  val data: LinkFiveVerifiedPurchases
+)
+
+data class LinkFiveVerifiedPurchases(
+  val purchases: List<LinkFiveVerifiedReceipt>
+)
+
+data class LinkFiveVerifiedReceipt(
+  val sku: String,
+  var purchaseId: String,
+  val transactionDate: String,
+  var validUntilDate: String,
+  var isTrial: Boolean,
+  val isExpired: Boolean,
+  var familyName: String? = null,
+  var attributes: String? = null,
+  var period: String? = null
+)
+
 class LaunchSdkNeededException(message: String = "Launch SDK needed.") : Exception(message)
 class NoActivityFoundException(message: String = "No activity found.") : Exception(message)
 class ProductIdNotFoundException(message: String = "Given product id does not exist") : Exception(message)
@@ -275,6 +411,7 @@ class NoProductsFoundException(message: String = "No products found") : Exceptio
 class CantMakePaymentsException(message: String = "Payment went wrong") : Exception(message)
 class DeviceNotSupportedException(message: String = "Device not supported.") : Exception(message)
 class UserCancelledPaymentException(message: String = "User cancelled payment.") : Exception(message)
+class NoPurchaseFoundException(message: String = "No purchase found.") : Exception(message)
 
 object Logger {
 
